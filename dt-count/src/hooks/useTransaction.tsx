9eba69311825fec3d870238b71ee87497de8a701
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-07-31 12:06:27 -0300

    finalizar o hook use transaction

--- dt-count/src/hooks/useTransaction.tsx
+++ dt-count/src/hooks/useTransaction.tsx
@@ -18,3 +18,49 @@ type TransactionInput = Omit <Transaction, "id" | "createdAt">
 interface TransactionProviderProps {
   children: ReactNode;
 }
+
+interface TransactionContextData {
+    transactions: Transaction[];
+    createTransaction:(transaction :TransactionInput) => Promise<void>;
+  }
+  
+  const TransactionsContext = createContext<TransactionContextData>({} as TransactionContextData) 
+  
+  // Criando um provider com todas as informacoes de transacciones
+  
+  export function TransactionsProvider ({children}:TransactionProviderProps){
+      const [transactions, setTransactions] = useState<Transaction[]>([])
+  
+      useEffect(()=>{
+        api.get('/transactions')
+        .then(response => setTransactions(response.data.transactions))
+      },[])  
+  
+      // Funcao que pega toda a lógica de criacao das transacoes 
+  
+      async function createTransaction(transactionInput: TransactionInput){
+        const response = await api.post('/transactions', {
+          ...transactionInput,
+          createdAt : new Date()
+        })
+        const {transaction} = response.data
+  
+        setTransactions([
+          ...transactions,
+          transaction
+        ])
+  
+  
+      }
+  
+      return(
+        <TransactionsContext.Provider value ={{transactions, createTransaction}}>
+          {children}
+        </TransactionsContext.Provider>
+      )
+  }
+  
+  export function useTransaction(){
+    const context = useContext(TransactionsContext)
+    return context
+} 
